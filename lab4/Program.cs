323396aaf1ea7ee87c3d6dabfa0c049fbcf6bb20
Ilya <ilya-gafarov2013@yandex.ru>
2021-12-04 00:52:08 +0400

    добавлена обработка ошибок

--- lab4/Program.cs
+++ lab4/Program.cs
@@ -11,6 +11,11 @@ namespace lab4
             string path = "";
             Console.WriteLine("Введите путь к файлу. Пример: ftp://ftp.mccme.ru/pub/metapost.README");
             path = Console.ReadLine();
+            if (!path.StartsWith("ftp://"))
+            {
+                Console.WriteLine("Указана некорректная ссылка.");
+                return;
+            }
             FtpWebRequest request = (FtpWebRequest)WebRequest.Create(path);
             request.Method = WebRequestMethods.Ftp.DownloadFile;
 
@@ -18,22 +23,28 @@ namespace lab4
             //request.Credentials = new NetworkCredential("login", "password");
             //request.EnableSsl = true; // если используется ssl
 
-            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
-            Stream responseStream = response.GetResponseStream();
-            // сохраняем файл
-            FileStream fs = new FileStream("fileTest.txt", FileMode.Create);
-
-            byte[] buffer = new byte[64];
-            int size = 0;
-            while ((size = responseStream.Read(buffer, 0, buffer.Length)) > 0)
+            try
             {
-                fs.Write(buffer, 0, size);
-            }
-            fs.Close();
-            response.Close();
+                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+                Stream responseStream = response.GetResponseStream();
+                // сохраняем файл
+                FileStream fs = new FileStream("fileTest.txt", FileMode.Create);
 
-            Console.WriteLine("Загрузка и сохранение файла завершены");
-            Console.ReadKey();
+                byte[] buffer = new byte[64];
+                int size = 0;
+                while ((size = responseStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    fs.Write(buffer, 0, size);
+                }
+                fs.Close();
+                response.Close();
+                Console.WriteLine("Загрузка и сохранение файла завершены");
+                Console.ReadKey();
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
